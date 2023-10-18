3eeb9ff12d984bb1cd47079c8578a0f6913a6406
Srikanth <srikanth@vestedfinance.co>
2023-10-18 09:57:02 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
+import { API_BASE_URL } from "../../secrets.json";
+import Image from "next/image";
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  const generatePDF = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(`${API_BASE_URL}/generate-pdf`);
+
+      window.open(data.url, "_blank");
+    } catch (err) {
+      alert("Error in generating PDF!!");
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div className="App">
+      <div className="print" onClick={generatePDF}>
+        {!loading && (
+          <Image
+            src="/printer.svg"
+            alt="Printer Logo"
+            width={24}
+            height={24}
+            priority
+          />
+        )}{" "}
+        <span className="printText">{loading ? "Loading" : "Print"}</span>
+      </div>
+    </div>
+  );
+};
+
+export default App;
